c32d6a2d677fc7b5d2d4aebbec019112b9b99ee3
norio c <norionorioc@gmail.com>
2020-02-23 04:36:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ class CashRegister
 
   attr_accessor :total, :discount,:cost, :items 
 
-  def initialize
+  def initialize(discount = 0)
       @total = 0
       @discount = discount 
       @items = [] 
